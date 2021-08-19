9ee0bfccc38a046873af2e65f8e635f067d4cbb6
XiangQianTom <XiangQianTom@users.noreply.github.com>
2021-08-19 20:00:31 +0800

    Update main.py
    步数修改范围40001,98800

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def main(user, passwd, step):
         return "user and passwd not empty！"
     
     if step == '':
-        print ("已设置为随机步数（24000-25000）")
-        step = str(random.randint(24000,25000))
+        print ("已设置为随机步数（40001-98800）")
+        step = str(random.randint(40001,98800))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
